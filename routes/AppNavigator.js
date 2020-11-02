bc7504420b74364ec2311f90e74e62b81a302114
Otonye Bestman <oerebestman@yahoo.com>
2020-11-02 15:33:46 +0000

    project upload

--- routes/AppNavigator.js
+++ routes/AppNavigator.js
@@ -2,14 +2,14 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "../screens/Homepage";
-import ReviewDetails from "../screens/Detailspage";
+import Details from "../screens/Detailspage";
 
 const { Navigator, Screen } = createStackNavigator();
 
 const HomeNavigator = () => (
   <Navigator>
     <Screen name="Home" component={Home} />
-    <Screen name="Details" component={ReviewDetails} />
+    <Screen name="Details" component={Details} />
   </Navigator>
 );
 
